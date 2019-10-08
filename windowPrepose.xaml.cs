7592e6d958fb1048f8cb89dafccabcf265e76e21
Sebastian <tallernait@gmail.com>
2019-10-07 21:49:29 -0400

    Fenetre Medecin finit. Fenetre Prepose, Elle recherche l'utilisateur et remplise les champ selon l'information de la base de donnee.

--- windowPrepose.xaml.cs
+++ windowPrepose.xaml.cs
@@ -22,6 +22,7 @@ namespace HospitalNL
         public windowPrepose()
         {
             InitializeComponent();
+           
         }
 
         private void btnLogout_Click(object sender, RoutedEventArgs e)
@@ -30,5 +31,84 @@ namespace HospitalNL
             main.Show();
             this.Close();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnUpdate.IsEnabled = false;
+            cbNSSDossier.ItemsSource = MainWindow.bdHospital.Patient.ToList();
+            cbIDMedecinDossier.ItemsSource = MainWindow.bdHospital.Medecin.ToList();
+        }
+
+        private void btnRecheche_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Patient pa = MainWindow.bdHospital.Patient.Where(p => p.Telephone == txtRecherchePatient.Text).FirstOrDefault();
+
+                if (pa == null)
+                    throw new Exception("Patient non trouve sur la base de donnee");
+                MessageBox.Show("Voila! On a trouve le patient.","Recherche avec succes", MessageBoxButton.OK);
+                btnUpdate.IsEnabled = true;
+                btnAjouterPatiet.IsEnabled = false;
+                dateNaissancePatient.SelectedDate = pa.DateNaissance;
+                txtNomPatient.Text = pa.Nom;
+                txtPrenomPatient.Text = pa.Prenom;
+                txtAdressePatient.Text = pa.Adresse;
+                txtProvincePatient.Text = pa.Province;
+                txtVillePatient.Text = pa.Ville;
+                txtCodePostalPatient.Text = pa.CodePostal;
+                txtTelePatient.Text = pa.Telephone;
+                cbAssurancePatient.ItemsSource = MainWindow.bdHospital.CompagnieAssurance.ToList();
+                cbRefPatient.ItemsSource = MainWindow.bdHospital.Parent.ToList();
+                cbAssurancePatient.SelectedItem = pa.CompagnieAssurance;
+                cbRefPatient.SelectedItem = pa.RefParent;
+   
+                Parent parent = MainWindow.bdHospital.Parent.Where(obj => obj.RefParent == (int)pa.RefParent).FirstOrDefault();
+                if (parent == null)
+                    throw new Exception("Le parent du patient n'exite pas sur la base de donnee");
+                txtNomParent.Text = parent.Nom;
+                txtPrenomParent.Text = parent.Prenom;
+                txtAdresseParent.Text = parent.Adresse;
+                txtTeleParent.Text = parent.Telephone;
+                txtProvinceParent.Text = parent.Province;
+                txtVilleParent.Text = parent.Ville;
+                txtCodeParent.Text = parent.CodePostal;
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Oups, on a de problemes!", MessageBoxButton.OK, MessageBoxImage.Information);
+                btnUpdate.IsEnabled = false;
+                btnAjouterPatiet.IsEnabled = true;
+                clearAll();
+            }
+            
+
+        }
+
+        private void clearAll()
+        {
+            foreach (Control ctl in containerGroupe.Children)
+            {
+                if (ctl.GetType() == typeof(CheckBox))
+                    ((CheckBox)ctl).IsChecked = false;
+                if (ctl.GetType() == typeof(TextBox))
+                    ((TextBox)ctl).Text = String.Empty;
+                if (ctl.GetType() == typeof(DatePicker))
+                    ((DatePicker)ctl).SelectedDate = null;
+                if (ctl.GetType() == typeof(ComboBox))
+                    ((ComboBox)ctl).SelectedIndex = -1;
+
+            }
+        }
+
+        private void btnEmptyBox_Click(object sender, RoutedEventArgs e)
+        {
+            clearAll();
+            btnUpdate.IsEnabled = false;
+        }
+
+        
     }
 }
